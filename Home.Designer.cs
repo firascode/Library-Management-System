e4cdbb23b2b2067432a49549a7e499b474f3ab3b
Firas Namazi <52861541+firasonly@users.noreply.github.com>
2020-04-16 21:06:44 +0300

    Home Design

---
+++ Home.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace MINI_PROJECT_LMS___FINAL_v
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.hom1 = new MINI_PROJECT_LMS___FINAL_v.hom();
+            this.rtr1 = new MINI_PROJECT_LMS___FINAL_v.rtr();
+            this.borr1 = new MINI_PROJECT_LMS___FINAL_v.borr();
+            this.srch1 = new MINI_PROJECT_LMS___FINAL_v.srch();
+            this.modi1 = new MINI_PROJECT_LMS___FINAL_v.modi();
+            this.add1 = new MINI_PROJECT_LMS___FINAL_v.add();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.Location = new System.Drawing.Point(913, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(133, 63);
+            this.button6.TabIndex = 12;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(785, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(133, 63);
+            this.button5.TabIndex = 11;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(609, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 63);
+            this.button4.TabIndex = 10;
+            this.button4.UseCompatibleTextRendering = true;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(479, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 63);
+            this.button3.TabIndex = 9;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(352, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 63);
+            this.button2.TabIndex = 8;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(181, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 63);
+            this.button1.TabIndex = 7;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1174, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(76, 41);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(-1, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(176, 82);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_1);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(12, 530);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(133, 117);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 130;
+            this.pictureBox3.TabStop = false;
+            // 
+            // hom1
+            // 
+            this.hom1.BackColor = System.Drawing.Color.Gold;
+            this.hom1.Location = new System.Drawing.Point(12, 152);
+            this.hom1.Name = "hom1";
+            this.hom1.Size = new System.Drawing.Size(1238, 524);
+            this.hom1.TabIndex = 19;
+            // 
+            // rtr1
+            // 
+            this.rtr1.BackColor = System.Drawing.Color.Gold;
+            this.rtr1.Location = new System.Drawing.Point(12, 152);
+            this.rtr1.Name = "rtr1";
+            this.rtr1.Size = new System.Drawing.Size(1238, 524);
+            this.rtr1.TabIndex = 17;
+            // 
+            // borr1
+            // 
+            this.borr1.BackColor = System.Drawing.Color.Gold;
+            this.borr1.Location = new System.Drawing.Point(12, 152);
+            this.borr1.Name = "borr1";
+            this.borr1.Size = new System.Drawing.Size(1238, 524);
+            this.borr1.TabIndex = 16;
+            // 
+            // srch1
+            // 
+            this.srch1.BackColor = System.Drawing.Color.Gold;
+            this.srch1.Location = new System.Drawing.Point(12, 152);
+            this.srch1.Name = "srch1";
+            this.srch1.Size = new System.Drawing.Size(1238, 524);
+            this.srch1.TabIndex = 15;
+            // 
+            // modi1
+            // 
+            this.modi1.BackColor = System.Drawing.Color.Gold;
+            this.modi1.Location = new System.Drawing.Point(12, 152);
+            this.modi1.Name = "modi1";
+            this.modi1.Size = new System.Drawing.Size(1238, 524);
+            this.modi1.TabIndex = 14;
+            // 
+            // add1
+            // 
+            this.add1.BackColor = System.Drawing.Color.Gold;
+            this.add1.Location = new System.Drawing.Point(12, 152);
+            this.add1.Name = "add1";
+            this.add1.Size = new System.Drawing.Size(1238, 524);
+            this.add1.TabIndex = 13;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.hom1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.rtr1);
+            this.Controls.Add(this.borr1);
+            this.Controls.Add(this.srch1);
+            this.Controls.Add(this.modi1);
+            this.Controls.Add(this.add1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private add add1;
+        private modi modi1;
+        private srch srch1;
+        private borr borr1;
+        private rtr rtr1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private hom hom1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
