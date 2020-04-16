8b254bfafa78d2e914ade58a6b4cb9f556b1046c
Firas Namazi <52861541+firasonly@users.noreply.github.com>
2020-04-16 21:06:18 +0300

    Home Page
    In this form, you will see the most borrowed book in the
     library and many functions as well.

---
+++ Home page.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MINI_PROJECT_LMS___FINAL_v
+{
+    public partial class hom : UserControl
+    {
+        public hom()
+        {
+            InitializeComponent();
+        }
+    }
+}
